8f4aa5e52f27c1c93d45aaee82a0a8879ec843ab
MichalWin10Pro <michalidesoft@gmail.com>
2021-12-03 04:09:59 +0100

    small cleanup

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import io
-
 from flask import Flask, jsonify
-from flask import render_template, request, send_file
+from flask import render_template, request
 import filetype
 from PIL import Image, ImageOps
-from io import BytesIO
 import base64
 
 UPLOAD_FOLDER = '/tmp'
@@ -20,44 +18,23 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def process_file():
-    # print(request.data)
-    # print("form",request.form)
-    # print("files",request.files)
-    # print("data",request.data)
-    # print("values",request.values)
-    # if request.method == 'POST':
-    #     f = request.files['the_file']
 
     image = request.files["file"]
-    print("TT", image.filename)
-    print("Ta", image.name)
-    print("Tnb", image.stream)
-    # image = request.data
+
     if filetype.is_image(image):
         print(f"{image} is a valid image...")
         img = Image.open(image)
-        # im1 = Image.open(r"C:\Users\System-Pc\Desktop\a.JPG")
         im2 = ImageOps.grayscale(img)
-        img_io = BytesIO()
         img_io2 = io.BytesIO()
-        im2.save(img_io, 'png')
         im2.save(img_io2, 'png')
-        img_io.seek(0)
         img_io2.seek(0)
         img_base64 = base64.b64encode(img_io2.read())
-        # return jsonify({'status': str(img_base64)})
-        # im3 = im2.save('output.png')
-        # return send_file(download_name=im3)
         return jsonify({
                    'msg': 'success',
                    'size': [im2.width, im2.height],
-
                    'img':  str(img_base64)
               })
-        # return send_file(img_io,
-        #                  attachment_filename='output.png',
-        #                  mimetype='image/png')
-    return jsonify({'file': "a"})
+    return jsonify({'msg': "not good file"})
 
 
 if __name__ == '__main__':
