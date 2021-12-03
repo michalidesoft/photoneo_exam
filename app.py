6993bc2eb4b7ff2926e34b37cea4b69ef2cacf56
MichalWin10Pro <michalidesoft@gmail.com>
2021-12-03 02:32:47 +0100

    greyscale file upload finally

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import io
+
 from flask import Flask, jsonify
 from flask import render_template, request, send_file
 import filetype
 from PIL import Image, ImageOps
 from io import BytesIO
+import base64
 
 UPLOAD_FOLDER = '/tmp'
 
@@ -36,13 +39,24 @@ def process_file():
         # im1 = Image.open(r"C:\Users\System-Pc\Desktop\a.JPG")
         im2 = ImageOps.grayscale(img)
         img_io = BytesIO()
+        img_io2 = io.BytesIO()
         im2.save(img_io, 'png')
+        im2.save(img_io2, 'png')
         img_io.seek(0)
+        img_io2.seek(0)
+        img_base64 = base64.b64encode(img_io2.read())
+        # return jsonify({'status': str(img_base64)})
         # im3 = im2.save('output.png')
         # return send_file(download_name=im3)
-        return send_file(img_io,
-                         attachment_filename='output.png',
-                         mimetype='image/png')
+        return jsonify({
+                   'msg': 'success',
+                   'size': [im2.width, im2.height],
+
+                   'img':  str(img_base64)
+              })
+        # return send_file(img_io,
+        #                  attachment_filename='output.png',
+        #                  mimetype='image/png')
     return jsonify({'file': "a"})
 
 
